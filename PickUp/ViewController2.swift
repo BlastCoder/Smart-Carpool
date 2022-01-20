822f4aa5dac213cad5aec41b8c672a1624ace4c6
Krish Patel <krishpatel@casady.org>
2022-01-20 12:48:52 -0600

    Added a TableView

--- PickUp/ViewController2.swift
+++ PickUp/ViewController2.swift
@@ -7,17 +7,31 @@
 
 import UIKit
 
-class ViewController2: UIViewController {
+class ViewController2: UIViewController, UITableViewDataSource {
+    
+    let tableViewData = Array(repeating: "Item", count: 5)
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tableViewData.count
+
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell",
+                                                     for: indexPath)
+            cell.textLabel?.text = self.tableViewData[indexPath.row]
+            return cell
+    }
 
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         title = "Nav-Bar"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "TableViewCell")
         // Do any additional setup after loading the view.
     }
     
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -25,6 +39,4 @@ class ViewController2: UIViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
-
 }
--- PickUp/ViewController3.swift
+++ PickUp/ViewController3.swift
@@ -31,31 +31,6 @@ class ViewController3: UIViewController{
     }
     
     
-    
-    
-    
-    
-    
-    /*
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = UITableViewCell()
-        cell.textLabel?.text = "test"
-        return cell
-    }
-    
-        // Do any additional setup after loading the view.
-        
-    }
-    */
-
     /*
     // MARK: - Navigation
 
